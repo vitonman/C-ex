5363fb76b05466d1375f98956ef1bcf97515ee05
vitodon <45338513+vitonman@users.noreply.github.com>
2019-07-28 22:06:49 +0300

    refactor test items

---
+++ ConsoleApp5/CycleClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class CycleClass
+    {
+
+        public void GetForCycle()
+        {
+
+            // Enter numbers
+            int[] nums = new int[7];
+            Console.WriteLine("Write seven numbers");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                Console.Write("{0} number: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+
+            }
+
+            // Sorting numbers 
+
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+
+                    if (nums[i] > nums[j])
+                    {
+
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+
+                    }
+
+                }
+
+            }
+
+            //output
+
+            Console.WriteLine("Output sorted massive");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                Console.WriteLine(nums[i]);
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+         
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,55 +11,9 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
 
-            /*
-                        // Enter numbers
-                        int[] nums = new int[7];
-                        Console.WriteLine("Write seven numbers");
+            CycleClass cycleclass = new CycleClass();
+            cycleclass.GetForCycle();
 
-                        for(int i = 0; i<nums.Length; i++)
-                        {
-
-                            Console.Write("{0} number: ", i + 1);
-                            nums[i] = Int32.Parse(Console.ReadLine());
-
-                        }
-
-                        // Sorting numbers 
-
-                        int temp;
-                        for (int i = 0; i < nums.Length-1; i++)
-                        {
-
-                            for (int j = i + 1; j < nums.Length; j++)
-                            {
-
-                                if (nums[i] > nums[j])
-                                {
-
-                                    temp = nums[i];
-                                    nums[i] = nums[j];
-                                    nums[j] = temp;
-
-                                }
-
-                            }
-
-                        }
-
-                        //output
-
-                        Console.WriteLine("Output sorted massive");
-
-                        for (int i = 0; i < nums.Length; i++)
-                        {
-
-                            Console.WriteLine(nums[i]);
-
-                        }
-
-                        Console.ReadLine();
-
-            */
 
             Person person = new Person();
             person.GetInfo();
@@ -73,7 +27,10 @@ namespace ConsoleApp5
 
             Console.ReadKey();
 
+            
         }
+
+
     }
 
     class Person
